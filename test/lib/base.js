6edda259fc6e5c9c6bfd172e762f58bd5cba948f
ddchen <842914439@qq.com>
2015-12-28 13:40:02 +0800

    add test

--- test/lib/base.js
+++ test/lib/base.js
@@ -13,11 +13,11 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 require('babel-polyfill');
 
 describe('processor', function () {
-    it('simple interflow', _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+    it('simple interflow', _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
         var methodFinder, del, connect, cal, res;
-        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
             while (1) {
-                switch (_context2.prev = _context2.next) {
+                switch (_context.prev = _context.next) {
                     case 0:
                         methodFinder = function methodFinder() {
                             return function (a, b) {
@@ -26,44 +26,77 @@ describe('processor', function () {
                         };
 
                         del = _index.processor.dealer()(methodFinder);
-
-                        connect = (function () {
-                            var _this = this;
-
-                            var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(rawIn) {
-                                return regeneratorRuntime.wrap(function _callee$(_context) {
-                                    while (1) {
-                                        switch (_context.prev = _context.next) {
-                                            case 0:
-                                                _context.next = 2;
-                                                return del(rawIn);
-
-                                            case 2:
-                                                return _context.abrupt('return', _context.sent);
-
-                                            case 3:
-                                            case 'end':
-                                                return _context.stop();
-                                        }
-                                    }
-                                }, _callee, _this);
-                            }));
-
-                            return function connect(_x) {
-                                return ref.apply(this, arguments);
-                            };
-                        })();
-
+                        connect = del;
                         cal = _index.processor.caller()(connect);
-                        _context2.next = 6;
+                        _context.next = 6;
                         return cal(1, 2);
 
                     case 6:
-                        res = _context2.sent;
+                        res = _context.sent;
 
                         _assert2.default.equal(res, 3);
 
                     case 8:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, undefined);
+    })));
+
+    it('map function', _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+        var methodMap, unpackIn, packOut, methodFinder, deal, callGen, res;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+            while (1) {
+                switch (_context2.prev = _context2.next) {
+                    case 0:
+                        methodMap = {
+                            'add': function add(a, b) {
+                                return a + b;
+                            }
+                        };
+
+                        unpackIn = function unpackIn(v) {
+                            return v[1];
+                        };
+
+                        packOut = function packOut(v) {
+                            return v;
+                        };
+
+                        methodFinder = function methodFinder(rawIn) {
+                            return methodMap[rawIn[0]];
+                        };
+
+                        deal = _index.processor.dealer(unpackIn, packOut)(methodFinder);
+
+                        callGen = function callGen(type) {
+                            var packIn = function packIn() {
+                                for (var _len = arguments.length, ins = Array(_len), _key = 0; _key < _len; _key++) {
+                                    ins[_key] = arguments[_key];
+                                }
+
+                                return [type, ins];
+                            };
+                            var unpackOut = function unpackOut(v) {
+                                return v;
+                            };
+
+                            var connect = deal;
+
+                            var call = _index.processor.caller(packIn, unpackOut)(connect);
+                            return call;
+                        };
+
+                        _context2.next = 8;
+                        return callGen('add')(1, 2);
+
+                    case 8:
+                        res = _context2.sent;
+
+                        _assert2.default.equal(res, 3);
+
+                    case 10:
                     case 'end':
                         return _context2.stop();
                 }
--- test/src/base.js
+++ test/src/base.js
@@ -7,12 +7,35 @@ describe('processor', () => {
         let methodFinder = () => (a, b) => a + b;
         let del = processor.dealer()(methodFinder);
 
-        let connect = async (rawIn) => {
-            return await del(rawIn);
-        };
+        let connect = del;
+
         let cal = processor.caller()(connect);
 
         let res = await cal(1, 2);
+        assert.equal(res, 3);
+    });
+
+    it('map function', async () => {
+        let methodMap = {
+            'add': (a, b) => a + b
+        };
+        let unpackIn = v => v[1];
+        let packOut = v => v;
+        let methodFinder = (rawIn) => methodMap[rawIn[0]];
+
+        let deal = processor.dealer(unpackIn, packOut)(methodFinder);
+
+        let callGen = (type) => {
+            let packIn = (...ins) => [type, ins];
+            let unpackOut = v => v;
+
+            let connect = deal;
+
+            let call = processor.caller(packIn, unpackOut)(connect);
+            return call;
+        };
+
+        let res = await callGen('add')(1, 2);
         assert.equal(res, 3);
     });
 });
