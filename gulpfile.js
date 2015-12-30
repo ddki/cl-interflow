9aba3165e33dd7ab8423fe01dd5692dea949ceb3
ddchen <842914439@qq.com>
2015-12-30 11:10:44 +0800

    update lint

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,26 @@ var babel = require('gulp-babel');
 var del = require('del');
 var exec = require('child_process').exec;
 
+var execp = function (cmd, cb) {
+    exec(cmd, {}, function (err, stdout, stderr) {
+        if(stdout) {
+            gutil.log(stdout);
+        }
+        if(stderr) {
+            gutil.log(stderr);
+        }
+        cb();
+    });
+};
+
 gulp.task('default', ['clean', 'test']);
 
 gulp.task('clean', function (cb) {
     del(['lib/**/*', 'test/lib/**/*'], cb);
+});
+
+gulp.task('lint', function (cb) {
+    execp('npm run lint', cb);
 });
 
 gulp.watch('src/**/*.js', ['test']);
@@ -33,25 +49,9 @@ gulp.task('buildTest', function () {
 });
 
 gulp.task('test', ['build'], function (cb) {
-    exec('mocha test/lib/**/*.js', {}, function (err, stdout, stderr) {
-        if(stdout) {
-            gutil.log(stdout);
-        }
-        if(stderr) {
-            gutil.log(stderr);
-        }
-        cb();
-    });
+    execp('mocha test/lib/**/*.js', cb);
 });
 
 gulp.task('cover', ['build'], function (cb) {
-    exec('npm test', {}, function (err, stdout, stderr) {
-        if(stdout) {
-            gutil.log(stdout);
-        }
-        if(stderr) {
-            gutil.log(stderr);
-        }
-        cb();
-    });
+    execp('npm test', cb);
 });
